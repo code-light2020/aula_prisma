fa9863da1bc67f8a5ad1aa531220e78066afa432
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2021-12-01 20:03:45 +0100

    message end

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import { MessageController } from "./controller/MessageController";
 import { OauthUserController } from "./controller/OauthUserController";
+import { isAuthenticated } from "./middleware/OauthMiddleware";
 
 const route = Router();
 route.post("/authenticate", new OauthUserController().handle)
+route.post("/message", isAuthenticated,new MessageController().handle)
 
 export { route }
--- src/service/MessageService.ts
+++ src/service/MessageService.ts
@@ -1,5 +1,16 @@
-export class MessageSevice{
-    async excute(message:string){
-        
+import { client } from "../db/prismaClient";
+
+export class MessageSevice {
+    async excute(text: string, user_id: string) {
+        const message = client.message.create({
+            data: {
+                text,
+                user_id
+            },
+            include: {
+                user: true
+            }
+        })
+        return message;
     }
 }
