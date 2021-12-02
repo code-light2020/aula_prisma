0476fbf9feb676d6d77316bca1b8d7296f33e315
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2021-12-02 01:48:40 +0100

    new change

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,17 @@ import cors from "cors"
 import { Server } from "socket.io"
 import { route } from "./router";
 const app = express();
+app.use(cors())
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_HOST
+    origin: "*"
   }
 });
 
 io.on("connection", socket => {
   console.log(`Usuario connectado no socket ${socket.id}`)
 })
-app.use(cors({ origin: process.env.FRONTEND_HOST }))
 app.use(express.json())
 app.use(route);
 app.get("/github", (_, res) => {
