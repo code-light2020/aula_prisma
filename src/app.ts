fd27481da4c817e939bfd045109621a61f48287f
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2021-12-02 01:41:23 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,14 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "*"
+    origin: process.env.FRONTEND_HOST
   }
 });
 
-io.on("connection",socket=>{
+io.on("connection", socket => {
   console.log(`Usuario connectado no socket ${socket.id}`)
 })
-app.use(cors())
+app.use(cors({ origin: process.env.FRONTEND_HOST }))
 app.use(express.json())
 app.use(route);
 app.get("/github", (_, res) => {
@@ -27,5 +27,5 @@ app.get("/sigin/callback", (req, res) => {
   return res.json(code);
 })
 
-export  {httpServer,io}
+export { httpServer, io }
 
